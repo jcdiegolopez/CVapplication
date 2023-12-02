c0b3d88d8f60a89b08ff5b7b5ee7043f071a2b3f
Diego Lopez <111710248+jcdiegolopez@users.noreply.github.com>
2023-12-02 08:49:31 -0600

    general form style

--- cv-project/src/components/GeneralForm.jsx
+++ cv-project/src/components/GeneralForm.jsx
@@ -2,22 +2,33 @@
 export default function GeneralhtmlForm(){
     return(
         <div className="GeneralhtmlForm">
-            <form action="/submit_htmlForm" method="post">
+            <form action="/submit_htmlForm" method="post" className="general-form-style">
+            <h1 className="general-form-title">General Information</h1>
+            <hr/>
+            <br/>
+            <div>
+                <label htmlFor="nombre">Nombre:</label>
+                <input type="text" id="nombre" name="nombre" required/>
+            </div>
+            
 
-            <label htmlFor="nombre">Nombre:</label>
-            <input type="text" id="nombre" name="nombre" required/>
+            <div>
+                <label htmlFor="apellido">Apellido:</label>
+                <input type="text" id="apellido" name="apellido" required/>
+            </div>
+            
 
-    
-            <label htmlFor="apellido">Apellido:</label>
-            <input type="text" id="apellido" name="apellido" required/>
+            <div>
+                <label htmlFor="telefono">Teléfono:</label>
+                <input type="tel" id="telefono" name="telefono" pattern="[0-9]{10}" placeholder="Ej. 1234567890" required/>
+            </div>
+            
 
-
-            <label htmlFor="telefono">Teléfono:</label>
-            <input type="tel" id="telefono" name="telefono" pattern="[0-9]{10}" placeholder="Ej. 1234567890" required/>
-
-    
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" required/>
+            <div>
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" required/>
+            </div>
+            
 
 
             <button type="submit">Enviar</button>
