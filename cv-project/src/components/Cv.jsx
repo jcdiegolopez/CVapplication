49c5586d46c0605c9ffce249b2e72bb3e9f3250b
Diego Lopez <111710248+jcdiegolopez@users.noreply.github.com>
2023-12-05 11:53:41 -0600

    strong

--- cv-project/src/components/Cv.jsx
+++ cv-project/src/components/Cv.jsx
@@ -7,30 +7,29 @@ export default function Cv({data}){
             <div className="general-section">
                 <h1> {data.general.firstname} {data.general.lastname}</h1>
                 <ul>
-                    <li>{data.general.tel}</li>
-                    <li>{data.general.mail}</li>
+                    <li>+ {data.general.tel}</li>
+                    <li>+ {data.general.mail}</li>
                 </ul>
             </div>
         </div>
         <div className="right-side">
             <div className="study-section">
-                <h1>Study Information</h1>
-                <ul>
-                    <li>{data.study.school}</li>
-                    <li>{data.study.title}</li>
-                    <li>{data.study.dateStudy}</li>
-                </ul> 
+            <h1>Study Information</h1>
+            <ul>
+                <li><strong>School:</strong> {data.study.school}</li>
+                <li><strong>Title:</strong> {data.study.title}</li>
+                <li><strong>Date finished:</strong> {data.study.dateStudy}</li>
+            </ul>
             </div>
             <div className="practice-section">
-                <h1>Practice Information</h1>  
-                <ul>
-                    <li>{data.practice.company}</li>
-                    <li>{data.practice.position}</li>
-                    <li>{data.practice.responsability}</li>
-                    <li>{data.practice.started}</li>
-                    <li>{data.practice.finished}</li>
-
-                </ul>  
+            <h1>Practice Information</h1>  
+            <ul>
+                <li><strong>Company:</strong> {data.practice.company}</li>
+                <li><strong>Position:</strong> {data.practice.position}</li>
+                <li><strong>Responsability:</strong> {data.practice.responsability}</li>
+                <li><strong>Date started:</strong> {data.practice.started}</li>
+                <li><strong>Date finished:</strong> {data.practice.finished}</li>
+            </ul>
             </div>
         </div>
 
